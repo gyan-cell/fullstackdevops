5c9e4b9c0591db318908630f1b75a894988790bf
gyan-cell <jha07944@gmail.com>
2024-06-19 12:09:27 +0530

    Basic Maths Operation code

---
+++ javascript/maths/maths.js
@@ -0,0 +1,8 @@
+const newNumberObject = new Number(21);
+const newVariable = 20;
+console.log(newNumberObject.toExponential(2)); //2.10e+1
+console.log(newNumberObject.toFixed(2));// 21.00
+console.log(newNumberObject.toPrecision(2)); // 21
+console.log(newNumberObject.toString())// 21
+console.log(newNumberObject.valueOf())// 21
+console.log(newVariable.toPrecision(6))
