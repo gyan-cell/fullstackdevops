6aea3e5bbbf30a55c8e35dcb641ce1c6cad428a3
gyan-cell <jha07944@gmail.com>
2024-08-12 09:06:35 +0530

    nodejs nodejs : dns.lookup()

--- nodejs/server.js
+++ nodejs/server.js
@@ -7,6 +7,17 @@ const server = createServer((req, res) => {
   res.end('Hello World');
 })
 
+server.on('error', (err) => {
+  console.log(err);
+})
+
+// Dns Lookup Example
+import dns from 'node:dns';
+dns.lookup("google.com", (err, address, family) => {
+  console.log('address: %j family: IPv%s', address, family);
+  console.log(address)
+  console.log(family)
+})
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 })
