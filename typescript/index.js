e34a1a8fa976d16c413d13b243c965ccc0cc2bf6
gyan-cell <jha07944@gmail.com>
2024-09-11 15:00:52 +0530

    InterFace For Classes In javascript

--- typescript/index.js
+++ typescript/index.js
@@ -19,4 +19,70 @@ const user = {
     userName: "Abhishek",
     age: 22
 };
-//Here we Are Creating The Object user with properties userName and age
+//Here we Are Creating The Object user with properties userName and age by default the userName Of the Object User is of type 
+//string and age is of type number
+console.log(typeof user); // object
+console.log(typeof user.userName); // string
+console.log(typeof user.age); // number
+//
+// InterFace Declaration : 
+console.log("InterFace Declaration : ");
+;
+let newUser = {
+    name: "John Doe",
+    age: 13,
+    isMarried: false,
+    contactNo: [123456789, 9876543210],
+    hasKids: true,
+    display() {
+        console.log(this.name);
+        console.log(this.age);
+        console.log(this.isMarried);
+        console.log(this.contactNo);
+        console.log(this.hasKids);
+    },
+    canHaveDrivingLicense() {
+        if (this.age >= 18) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+//console.log(newUser)
+//console.log(typeof newUser);
+// Since Javascript Supports Class Based Programming We Can Create Our Own Class And Do It Like This :
+console.log("Class Based Programming : ");
+class UserEmp {
+    constructor(name, age, isMarried, contactNo, hasKids) {
+        this.name = name;
+        this.age = age;
+        this.isMarried = isMarried;
+        this.contactNo = contactNo;
+        this.hasKids = hasKids;
+    }
+    display() {
+        console.log(this.name);
+        console.log(this.age);
+        console.log(this.isMarried);
+        console.log(this.contactNo);
+        console.log(this.hasKids);
+    }
+    canHaveDrivingLicense() {
+        if (this.age >= 18) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+const newEmp = new UserEmp("John", 13, false, [123456789, 9876543210], true);
+if (newEmp.display) {
+    console.log("newEmp.display() is defined");
+    newEmp.display();
+}
+else {
+    console.log("newEmp.display() is undefined");
+}
